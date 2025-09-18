7802ee6c2011ecf02979d5b5ce0822af0dda5c34
intghp <gustavohlucenapaiva1@gmail.com>
2025-09-18 19:01:39 -0300

    fix: alterações no models.py

--- projeto/livros/models.py
+++ projeto/livros/models.py
@@ -4,8 +4,6 @@ from django.db import models
 
 class LivroModel(models.Model):
     titulo = models.CharField(max_length=250)
-    nome_autor = models.CharField(max_length=250)
-    ano_publicacao = models.IntegerField()
 
     def __str__(self):
         return self.titulo
