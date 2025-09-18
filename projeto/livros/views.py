cc53a39172568099a34f9f38f5927c28c08b6edd
intghp <gustavohlucenapaiva1@gmail.com>
2025-09-18 19:00:11 -0300

    fix: correções no views

--- projeto/livros/views.py
+++ projeto/livros/views.py
@@ -10,23 +10,22 @@ from .forms import LivroForm
 
 class LivroViewList(ListView):
     model = LivroModel
-    template_name = "listar_livros.html"
-    content_object_name = "livros"
+    template_name = "livros/listar_livros.html"
+    context_object_name = "livros"
 
 class LivroCreateView(CreateView):
     model = LivroModel
     form_class = LivroForm
-    template_name = "adicionar_livro.html"
-    success_url = reverse_lazy("listar_livro")
+    template_name = "livros/adicionar_livro.html"
+    success_url = reverse_lazy("listar_livros")
 
 class LivroUpdateView(UpdateView):
     model = LivroModel
     form_class = LivroForm
-    template_name = "atualizar_livro.html"
-    success_url = reverse_lazy("listar_livro")
+    template_name = "livros/atualizar_livro.html"
+    success_url = reverse_lazy("listar_livros")
 
 class LivroDeleteView(DeleteView):
     model = LivroModel
-    form_class = LivroForm
-    template_name = "deletar_livro.html"
-    success_url = reverse_lazy("listar_livro")
+    template_name = "livros/deletar_livro.html"
+    success_url = reverse_lazy("listar_livros")
