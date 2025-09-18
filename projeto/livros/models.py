f96b9a309ccb2b228f53d66deb527ac48f2932b0
Jonnathan Vituriano <viturianojonnathan@gmail.com>
2025-09-18 16:14:32 -0300

    feat: Adição dos models

--- projeto/livros/models.py
+++ projeto/livros/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class LivroModel(models.Model):
+    titulo = models.CharField(max_length=250)
+    nome_autor = models.CharField(max_length=250)
+    ano_publicacao = models.IntegerField()
+
+    def __str__(self):
+        return self.titulo
